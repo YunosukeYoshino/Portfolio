1e37a03910ab34d0681b86282546c362876227bf
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-03-20 19:52:41 +0900

    ⚡️ humburgerで付与するスタイルを更新しました。

--- src/assets/js/Common/Hamburger.ts
+++ src/assets/js/Common/Hamburger.ts
@@ -2,6 +2,16 @@ const Hamburger = () => {
   const menu = document.querySelector(".js-humburger") as HTMLElement;
   menu.addEventListener("click", function () {
     menu.classList.toggle("is-active");
+
+    if (menu.classList.contains("is-active")) {
+      document.body.style.setProperty("--mix-blend-mode", "none");
+      document.body.style.setProperty("position", "fixed");
+      document.body.style.setProperty("width", "100%");
+    } else {
+      document.body.style.removeProperty("--mix-blend-mode");
+      document.body.style.removeProperty("position");
+      document.body.style.removeProperty("width");
+    }
   });
 };
 
