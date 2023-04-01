342146e7bc9ff4e674ba1bf763a8193d8eae912c
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-04-02 01:45:30 +0900

    🔥 functionを調整

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,9 +1,6 @@
 import { setUpAccordion, Hamburger } from "./Common";
-import { TestAnimation } from "./Gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
-  TestAnimation(); //Gsap Test Animation
-
   setUpAccordion(); //Accordion
   Hamburger(); //Hamburger Menu
 });
