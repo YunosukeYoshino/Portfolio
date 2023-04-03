6ab1c486316c45af84fc774baf8a4195c3f6eac9
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-04-03 20:22:45 +0900

    🔥 loaderを実装しました。

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,6 +1,56 @@
+import gsap from "gsap";
 import { setUpAccordion, Hamburger } from "./Common";
-
+const TL = gsap.timeline();
 document.addEventListener("DOMContentLoaded", () => {
   setUpAccordion(); //Accordion
   Hamburger(); //Hamburger Menu
+
+  TL.to(".p-loader", {
+    autoAlpha: 0,
+    duration: 1.8,
+    ease: "back.out(1.7)", //ease
+  });
+
+  gsap.to(".js-main", {
+    autoAlpha: 1,
+    duration: 1.8,
+    ease: "back.out(1.7)", //ease
+  });
+  // const anchors = document.querySelectorAll<HTMLAnchorElement>("a[href]");
+
+  // anchors.forEach((anchor: HTMLAnchorElement) => {
+  //   anchor.addEventListener("click", (e: Event) => {
+  //     const href = anchor.getAttribute("href") ?? "";
+  //     e.preventDefault();
+  //     clickedAnchor();
+  //     setTimeout(() => {
+  //       window.location.href = href;
+  //     }, 100);
+  //   });
+  // });
+
+  // const clickedInternalLinks = () => {
+  //   const internalLinks = document.querySelectorAll<HTMLAnchorElement>('a[href^="/#"]');
+  //   internalLinks.forEach((anchor: HTMLAnchorElement) => {
+  //     anchor.addEventListener("click", (e: Event) => {
+  //       gsap.to(".js-main", {
+  //         autoAlpha: 1,
+  //         duration: 0.4,
+  //         ease: "back.out(1.7)", //ease
+  //       });
+  //     });
+  //   });
+  // };
+
+  // if (location.pathname == "/") {
+  //   clickedInternalLinks();
+  // }
 });
+
+// const clickedAnchor = () => {
+//   TL.to(".js-main", {
+//     autoAlpha: 0,
+//     duration: 0.4,
+//     ease: "back.out(1.7)", //ease
+//   });
+// };
